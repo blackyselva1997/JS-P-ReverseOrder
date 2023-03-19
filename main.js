f2434c635dd7efcdd10d192eddf352e68a71358a
blackyselva <selvam17196@gmail.com>
2023-03-19 17:30:33 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,12 @@
+document.write("2. Print N number in reverse order loop");
+
+document.write("<br>");
+document.write("<br>");
+
+var n=prompt("Enter a reverse N number");
+var i=n;
+while (i>=1) {
+	document.write(i);
+	document.write("<br>");
+	i -=1;
+}
